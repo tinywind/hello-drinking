6180f7d88e9c50c9233cc15221d48df1ee787196
jck <yeoun23th@naver.com>
2011-08-03 16:42:24 +0900

    ................

--- MoblieService/src/transmission/Sendable.java
+++
@@ -1,7 +0,0 @@
-package transmission;
-
-public interface Sendable {
-	public boolean login(String id, String pw);
-	public boolean logout(String id);
-	public boolean join(UserInfo info) throws TransmissionException;
-}
