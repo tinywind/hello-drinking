d15512279c60f76fa9fd49c0fad71b3dab52c0b9
Jeon Jae Heung <tinywind@naver.com>
2011-08-03 15:49:41 +0900

    .....

--- MoblieService/src/transmission/Sendable.java
+++ MoblieService/src/transmission/Sendable.java
@@ -3,5 +3,5 @@ package transmission;
 public interface Sendable {
 	public boolean login(String id, String pw);
 	public boolean logout(String id);
-	
+	public boolean join(UserInfo info) throws TransmissionException;
 }
