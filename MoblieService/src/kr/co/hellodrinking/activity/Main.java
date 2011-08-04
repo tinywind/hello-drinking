2370f1c104beaaa5043328b69d40ae43ed7255ab
Jeon Jae Heung <tinywind@naver.com>
2011-08-04 14:14:05 +0900

    ..

--- MoblieService/src/kr/co/hellodrinking/activity/Main.java
+++ MoblieService/src/kr/co/hellodrinking/activity/Main.java
@@ -5,16 +5,10 @@ import android.app.Activity;
 import android.os.Bundle;
 
 public class Main extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-<<<<<<< HEAD
-        setContentView(R.layout.member_join);
-
-=======
-        setContentView(R.layout.main);
-        
->>>>>>> branch 'master' of git@github.com:tinywind/HelloDrinking.git
-    }
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.memberjoin);
+	}
 }
