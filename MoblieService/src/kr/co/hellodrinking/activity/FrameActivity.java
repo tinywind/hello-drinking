a773f308b8a18fe32a7d05109b68981ab36ec892
Jeon Jae Heung <tinywind@naver.com>
2011-08-08 10:22:25 +0900

    .

--- MoblieService/src/kr/co/hellodrinking/activity/FrameActivity.java
+++ MoblieService/src/kr/co/hellodrinking/activity/FrameActivity.java
@@ -2,16 +2,21 @@ package kr.co.hellodrinking.activity;
 
 import kr.co.hellodrinking.R;
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 
 public abstract class FrameActivity extends Activity{
+	protected LayoutInflater inflater;
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.frame);
         
-        // R.id.frame_linear_content 에 invoke 시키는 과정이 필요
-        // loadContent() 애서 그 내용을 명시
+        inflater = (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        
+        loadContent();
     }
     
     abstract void loadContent();
--- MoblieService/src/kr/co/hellodrinking/activity/SampleFrameActivity.java
+++ MoblieService/src/kr/co/hellodrinking/activity/SampleFrameActivity.java
@@ -1,7 +1,12 @@
 package kr.co.hellodrinking.activity;
 
+import android.view.ViewGroup;
+import kr.co.hellodrinking.R;
+
 public class SampleFrameActivity extends FrameActivity{
 	@Override
 	void loadContent() {
+		inflater.inflate(R.layout.postslist, (ViewGroup) findViewById(R.id.frame_layout_contents));
+		
 	}
 }
