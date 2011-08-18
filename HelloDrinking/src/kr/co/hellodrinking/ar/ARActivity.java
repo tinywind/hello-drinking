4934a64afd66de5608362157716cc63343ebe3e7
Jeon Jae-Heung <tinywind@naver.com>
2011-08-18 17:53:21 +0900

    .

--- HelloDrinking/src/kr/co/hellodrinking/ar/ARActivity.java
+++ HelloDrinking/src/kr/co/hellodrinking/ar/ARActivity.java
@@ -2,6 +2,8 @@ package kr.co.hellodrinking.ar;
 
 import kr.co.hellodrinking.R;
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.Window;
 import android.widget.FrameLayout;
@@ -17,8 +19,9 @@ public class ARActivity extends Activity {
 		FrameLayout frame = (FrameLayout) findViewById(R.id.ar_frame);
 		
 		ImageButton imagebutton = new ImageButton(this);
+		Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.ic_map_arrive);
+		imagebutton.setImageBitmap(bitmap);
+		frame.addView(imagebutton, new FrameLayout.LayoutParams(FrameLayout.LayoutParams.WRAP_CONTENT,FrameLayout.LayoutParams.WRAP_CONTENT));
 		
 	}
-	
-	//NT-R710 AS240
 }
